8cc6e48269e1840c65f1422729080456306e365f
Amitpoonia16 <amit3dpoonia@gmail.com>
2020-09-24 16:05:47 +0530

    Check the product count on the first page

--- testproject/prapackege/productcount.py
+++ testproject/prapackege/productcount.py
@@ -1,3 +1,4 @@
+#Check the product count on the first page
 import time
 
 from selenium import webdriver
@@ -5,3 +6,9 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
+
+product_count = driver.find_elements_by_xpath("//div[@class='product']")
+product_count1 = len(product_count)
+print(product_count1)
+
+driver.close()
